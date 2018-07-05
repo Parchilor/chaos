448212516b14587bb1f8252e14cafc46cab20e67
Parchilor <837001983@qq.com>
2018-07-05 00:15:16 -0700

    struct

--- src/main.c
+++ src/main.c
@@ -1,23 +1,25 @@
-#include "c_inc.h"
+#include <math.h>
+#include <time.h>
+#include <errno.h>
+#include <stdio.h>
+#include <assert.h>
+#include <signal.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/utsname.h>
 
-int 
+#include "type.h"
+static DEV_INFO board_info[] = {
+	{DEV_ADD_INFO("Sony", "NW-ZX100", 0x01),},
+};
+
+int
 main (int argc, char *argv[])
 {
-	time_t t;
-	struct tm *tmp;
-	char buf1[16], buf2[64];
-	time (&t);
-	tmp = localtime (&t);
-	if (strftime (buf1, 16, "%X, %a %b %d, %Y", tmp) == 0)	{
-		printf ("Buffer length 16 is too small\n");
-	}	else	{
-		printf ("%s\n", buf1);
-	}
-
-	if (strftime (buf2, 64, "%X, %a %b %d, %Y", tmp) == 0)	{
-		printf ("Buffer length 64 is too small\n");
-	}	else	{
-		printf("%s\n", buf2);
-	}
+	printf("Company: %s, Type: %s, ID: 0x%.2x\n", board_info[0].company, board_info[0].type, board_info[0].id);
 	return 0;
 }
