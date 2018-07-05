01b6b3704c7239d2fe17cf49a76e6d3a32d2e591
Parchilor <837001983@qq.com>
2018-07-05 00:18:42 -0700

    Two dimensional pointer

--- src/main.c
+++ src/main.c
@@ -1,25 +1,18 @@
-#include <math.h>
-#include <time.h>
-#include <errno.h>
 #include <stdio.h>
-#include <assert.h>
-#include <signal.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <sys/types.h>
-#include <sys/utsname.h>
 
-#include "type.h"
-static DEV_INFO board_info[] = {
-	{DEV_ADD_INFO("Sony", "NW-ZX100", 0x01),},
-};
-
-int
-main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("Company: %s, Type: %s, ID: 0x%.2x\n", board_info[0].company, board_info[0].type, board_info[0].id);
+	printf("Testing!\n");
+	char *srcarr[4] = {
+		"East",
+		"West",
+		"North",
+		"South"
+	};
+	int i;
+	for(i = 0; i < 4; i++)
+	{
+		printf("%s\n", srcarr[i]);
+	}
 	return 0;
 }
