c2d91f970d60a8b237ac6f5857e1c70007e99df4
Parchilor <837001983@qq.com>
2018-07-05 00:24:11 -0700

    token

---
+++ include/c_inc.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <math.h>
+#include <time.h>
+#include <errno.h>
+#include <stdio.h>
+#include <assert.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/utsname.h>
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned int u32;
+typedef unsigned long u64;
--- src/main.c
+++ src/main.c
@@ -1,22 +1,101 @@
+#include "c_inc.h"
 #include <time.h>
-//#include <errno.h>
-#include <stdio.h>
-//#include <stdlib.h>
-//#include <string.h>
+#include <getopt.h>
+#include <unistd.h>
 
-int main(argc, argv)
-	int argc;
-	char *argv[];
+enum rdmode {
+	RSM_Capital = 1,
+	RSM_Letter,
+	RSM_Cap_Let,
+	RSM_Number,
+	RSM_Cap_Num,
+	RSM_Let_Num,
+	RSM_ALL,
+};
+
+char BC[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char SC[] = "abcdefghijklmnopqrstuvwxyz";
+char NO[] = "0123456789";
+
+struct option long_opts[] = {
+	{"len", required_argument, NULL,'l'},
+	{"mode", required_argument, NULL,'m'},
+	{0,0,0,0},
+};
+
+char 
+*CreateToken(int len, enum rdmode mode);
+
+int
+main (int argc, char *argv[])
 {
-	time_t t;
-	struct tm *tm;
-	time(&t);
-	tm = gmtime(&t);
-	printf("%.4d/%.2d/%.2d %.2d:%.2d:%.2d\n", 
-			1900+tm->tm_year, 
-			tm->tm_mon+1, 
-			tm->tm_mday, 
-			tm->tm_hour > 15?(tm->tm_hour - 16):(tm->tm_hour+8), 
-			tm->tm_min, 
-			tm->tm_sec);
+	int c, len, mode;
+	while((c = getopt_long(argc, argv, "l:m:", long_opts, NULL)) != -1){
+		switch(c){
+			case 'l':
+				len = atoi(optarg);
+				break;
+			case 'm':
+				mode = atoi(optarg);
+				break;
+		}
+	}
+	char *token = CreateToken(len, mode);
+	printf("%s\n", token);
+	return 0;
+}
+
+char 
+*CreateToken(int len, enum rdmode mode)
+{
+	int maxLen = 0, i, index;
+	char select[62];
+	char *str = malloc(sizeof(char) * len);
+
+
+	srand((unsigned)time(NULL));
+	switch(mode){
+		case RSM_Capital:
+			sprintf(select, "%s", BC);
+			maxLen = 26;
+			break;
+
+		case RSM_Letter:
+			sprintf(select, "%s", SC);
+			maxLen = 26;
+			break;
+
+		case RSM_Cap_Let:
+			maxLen = 52;
+			sprintf(select, "%s%s", SC, BC);
+			break;
+
+		case RSM_Number:
+			sprintf(select, "%s", NO);
+			maxLen = 10;
+			break;
+
+		case RSM_Cap_Num:
+			sprintf(select, "%s%s", NO, BC);
+			maxLen = 36;
+			break;
+
+		case RSM_Let_Num:
+			sprintf(select, "%s%s", NO, SC);
+			maxLen = 36;
+			break;
+
+		case RSM_ALL:
+			sprintf(select, "%s%s%s", NO, SC, BC);
+			maxLen = 62;
+			break;
+		default:
+			sprintf(select, "%s", BC);
+			maxLen = 26;
+	}
+	for(i = 0; i < len; i++){
+		index = rand()%(maxLen);
+		str[i] = select[index];
+	}
+	return str;
 }
