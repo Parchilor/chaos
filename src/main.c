17735cceea5e57cc6bf97c7a592517cc845b5f60
Parchilor <837001983@qq.com>
2018-07-07 02:46:42 -0700

    :struct

--- src/main.c
+++ src/main.c
@@ -1,15 +1,34 @@
 #include <stdio.h>
 
+typedef struct {
+	unsigned char a:5;
+	unsigned char b:3;
+	unsigned char c:1;
+	unsigned char d:7;
+	unsigned char e:2;
+} _BIT;
+
 #define STR(n) (#n)
-#define CONS(a, b) (a##b)
 
 int main(int argc, char *argv[])
 {
-	printf("%s->%ld\n", STR(char), sizeof(char));
-	printf("%s->%ld\n", STR(short int), sizeof(short int));
-	printf("%s->%ld\n", STR(int), sizeof(int));
-	printf("%s->%ld\n", STR(long int), sizeof(long int));
-	printf("%s->%ld\n", STR(double), sizeof(double));
+//	_BIT arg = {0xff, 0xff, 0xff, 0xff, 0xff};
+	_BIT arg = {0x00, 0x00, 0x00, 0x00, 0x00};
 
+	printf("The struct size: _%s ---> %ld\n\n", STR(_BIT), sizeof(_BIT));
+
+	printf("Store: %s ---> %d[0x%x]\n", STR(a), arg.a, arg.a);
+	printf("Store: %s ---> %d[0x%x]\n", STR(b), arg.b, arg.b);
+	printf("Store: %s ---> %d[0x%x]\n", STR(c), arg.c, arg.c);
+	printf("Store: %s ---> %d[0x%x]\n", STR(d), arg.d, arg.d);
+	printf("Store: %s ---> %d[0x%x]\n", STR(e), arg.e, arg.e);
+
+	arg.c = 0xff;
+
+	printf("Store: %s ---> %d[0x%x]\n", STR(a), arg.a, arg.a);
+	printf("Store: %s ---> %d[0x%x]\n", STR(b), arg.b, arg.b);
+	printf("Store: %s ---> %d[0x%x]\n", STR(c), arg.c, arg.c);
+	printf("Store: %s ---> %d[0x%x]\n", STR(d), arg.d, arg.d);
+	printf("Store: %s ---> %d[0x%x]\n", STR(e), arg.e, arg.e);
 	return 0;
 }
