361f3d09b33a02f95e94a896cb3f10e151c49a75
Parchilor <837001983@qq.com>
2018-07-21 15:27:31 +0800

    n power(0 to 32w)

--- src/main.c
+++ src/main.c
@@ -1,47 +1,21 @@
 #include <stdio.h>
 
-char in[128] = "init i2c pn adc\n";
+#define _BIT 32
+
+unsigned long int data_width(unsigned long int bit)
+{
+	unsigned long int i, sum = 1;
+	for(i = 0; i < bit; i++)
+	{
+		sum *= 2;
+	}
+	return sum;
+}
 
 int main(int argc, char *argv[])
 {
-	char *p_in = in;
-	int cnt = 0;
-	const char* params[16];
-	while(*p_in)
-	{
-	//	printf("%d ----> %s\n", cnt, *p_in);
-		switch(*p_in)
-		{
-			case '\r':
-			case '\n':
-			case '\t':
-			case ' ':
-				p_in++;
-				continue;
-		}
-		if(0 == *p_in)
-			break;
-		params[cnt] = p_in;
-		cnt++;
-		while(*p_in)
-		{
-			if ('\r' == *p_in ||'\n' == *p_in ||' '  == *p_in || '\t' == *p_in)
-			{
-				*p_in = '\0';
-				p_in++;
-				break;
-			}
-			p_in ++;
-		}
-	}
+	printf("Model Testing!\n");
 
-	int i;
-	printf("count = %d\n", cnt);
-	for(i = 0; i < cnt; i++)
-	{
-		printf("params[%d] = %s ", i, params[i]);
-	}
-	putchar('\n');
-	putchar('\n');
+	printf("%d-bit Maximum Decimalism: %lu\n", _BIT, data_width(_BIT));
 	return 0;
 }
