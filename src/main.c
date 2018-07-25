88524033e33f108680f2e1821ef44d3132272cb5
Parchilor <837001983@qq.com>
2018-07-25 17:22:38 +0800

    Integer rotate to wstring

--- src/main.c
+++ src/main.c
@@ -1,18 +1,48 @@
 #include <stdio.h>
-#define GYRO_AND_ACCEL_MODE 1
+#include <stdlib.h>
+void itoa(unsigned long val, char *buf, unsigned radix)
+{ 
+	char *p;           /* pointer to traverse string */ 
+	char *firstdig;       /* pointer to first digit */ 
+	char temp;           /* temp char */ 
+	unsigned digval;       /* value of digit */ 
+	
+	p = buf; 
+	firstdig = p;         /* save pointer to first digit */ 
+	
+	do{
+		digval = (unsigned) (val % radix); 
+		val /= radix;     /* get next digit */ 
+		
+		/* convert to ascii and store */ 
+		if (digval > 9) 
+			*p++ = (char)(digval - 10 + 'a');   /* a letter */ 
+		else 
+			*p++ = (char)(digval + '0');     /* a digit */ 
+	} while(val > 0); 
+	
+	/* We now have the digit of the number in the buffer, but in reverse 
+	   order.   Thus we reverse them now. */ 
+	
+	*p-- = '\0';         /* terminate string; p points to last digit */ 
+	
+	do{ 
+		temp = *p; 
+		*p = *firstdig; 
+		*firstdig = temp;   /* swap *p and *firstdig */ 
+		--p; 
+		++firstdig;       /* advance to next two digits */ 
+	} while (firstdig < p); /* repeat until halfway */ 
+}
+
+
+
+
 int main(int argc, char *argv[])
 {
-	printf("Model Testing!\n");
-
-	int res = 32;
-	int sub = -32;
-
-#if GYRO_MODE || \
-	ACCEL_MODE || \
-	GYRO_AND_ACCEL_MODE || \
-	ACCEL_AND_GYRO_MODE
-	
-	printf("Result: %d[0x%x]\n", res - sub, res - sub);
-#endif
+	int a = 255;
+	char s[10];
+	itoa(a, s, atoi(argv[1]));
+	printf("%s\n", s);
 	return 0;
 }
